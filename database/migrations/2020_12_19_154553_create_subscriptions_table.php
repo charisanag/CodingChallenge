bd2ad398bdf45df665adaf5b31f8e0941511a3d9
xarou94 <xaris.anag@gmail.com>
2020-12-19 18:46:22 +0200

    Initial Migrations

---
+++ database/migrations/2020_12_19_154553_create_subscriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->integer("subscription_type_id");
+            $table->uuid("user_id ")->primary();
+            $table->decimal("price",4);
+            $table->date("from");
+            $table->date("to");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
--- database/migrations/2020_12_19_162924_create_subscription_types_table.php
+++ database/migrations/2020_12_19_162924_create_subscription_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateSubscriptionTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('subscription_types', function (Blueprint $table) {
+            $table->string("name");
+            $table->decimal("price",4);
         });
     }
 
@@ -27,6 +26,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('subscription_types');
     }
 }
